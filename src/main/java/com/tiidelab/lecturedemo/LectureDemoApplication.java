417da8a85c7465c6c12310550794378f8f2f4c72
Peter Adeyemo <adeyemodanointed@yahoo.com>
2021-08-06 08:26:35 +0100

    spring boot initializer added

---
+++ src/main/java/com/tiidelab/lecturedemo/LectureDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tiidelab.lecturedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiidelab/lecturedemo/LectureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiidelab.lecturedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
